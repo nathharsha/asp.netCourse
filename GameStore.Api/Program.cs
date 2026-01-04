501d91de3dc01a6fd8c90fd0a1f4934bff04c70a
Harsha Nath <nathharsha@gmail.com>
2026-01-04 22:55:16 +1100

    Added checks when id not found during get game by id and update game

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -15,8 +15,13 @@ List<GameDto> games = [
 app.MapGet("games", () => games);
 
 //Get game by ID - /games/{id}
-app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id))
-    .WithName(GetGameEndpointName);
+app.MapGet("games/{id}", (int id) =>
+{
+    GameDto? game = games.Find(game => game.Id == id);
+
+    return game is null ? Results.NotFound() : Results.Ok(game);
+})
+.WithName(GetGameEndpointName);
 
 app.MapGet("games/test", () => "Hello its me again");
 
@@ -39,6 +44,11 @@ app.MapPost("games", (CreateGameDto newGame) =>
 app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) =>
 {
     var index = games.FindIndex(game => game.Id == id);
+
+    if (index == -1)
+    {
+        return Results.NotFound();
+    }
 
     games[index] = new GameDto(
         id,
